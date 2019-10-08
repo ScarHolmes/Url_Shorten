475229c55d0f92d2dba04ad8414536e2597b2ab9
scott-olson <scotteolson3@gmail.com>
2019-10-08 14:31:48 -0400

    Revert "add the framework for unit testing the alphabet and url generator"
    This reverts commit 920dd2c7d8066209d5c9ee974aad0a6a02703130.
    
    add the framework for unit testing the alphabet and url generator

--- urlShorten/tests.py
+++ urlShorten/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import unittest
+import alphabet
+import generate
+import string
 
-# Create your tests here.
+
+class TestAlphabet(unittest.TestCase):
+	# should return a full alphabet of valid characters
+	def test_get_alpha(self):
+		test_alpha = alphabet.Alphabet()
+		self.assertEqual(test_alpha.get(), list(string.ascii_letters + string.digits))
+		self.assertEqual(test_alpha.lookup(0), 'a')
+
+
+class TestGenerate(unittest.TestCase):
+	# work in progress
+	def test_generte_url(self):
+		self.assertEqual(0,0)
+
+if __name__ == '__main__':
+	unittest.main()
